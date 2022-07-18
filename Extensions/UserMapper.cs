8617edfa22bc4f43dd4cbb7c5ba270a19737713a
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-07-18 23:28:31 +0000

    user mapster for mapping to Dto

--- Extensions/UserMapper.cs
+++ Extensions/UserMapper.cs
@@ -1,19 +1,17 @@
+using Mapster;
 using Kolaczyn.Models;
 
 namespace Kolaczyn.Extensions;
 
 public static class UserMapper
 {
-  // should probably use library for this
   public static User ToDomain(this UserDto user)
   {
-    Sex sex;
-    Enum.TryParse(user.Sex, out sex);
-    return new User(user.Name, sex, user.Age);
+    return user.Adapt<User>();
   }
   public static UserDto ToDto(this User user)
   {
-    return new(user.Name, user.Id, user.Sex.ToString(), user.Age);
+    return user.Adapt<UserDto>();
   }
 }
 
