b4d2bce8cb6fb6b99f262570d35401b4af9c2772
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-07-19 18:02:45 +0000

    add init db endpoint

--- Controllers/MatchController.cs
+++ Controllers/MatchController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using Dapper;
 using Kolaczyn.Application.UseCases;
 using Kolaczyn.Domain.Model;
 using Kolaczyn.Application.Dto;
@@ -36,6 +38,21 @@ public class MatchController : ControllerBase
   public async Task<IEnumerable<User>> GetUsers([FromServices] GetUsersUseCase useCase)
   {
     return await useCase.Execute();
+  }
+
+  // TODO this is pretty dumb
+  [HttpGet("initDb")]
+  public async Task InitDb()
+  {
+
+    await using var connection = new NpgsqlConnection("Host=db;Username=postgres;Password=postgres;Database=postgres;Port=5432");
+    await connection.OpenAsync();
+    await using (var cmd = new NpgsqlCommand("CREATE TABLE users(id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, age INT NOT NULL)", connection))
+    {
+      var xd = await cmd.ExecuteReaderAsync();
+    };
+    connection.Close();
+
   }
 
 }
--- Program.cs
+++ Program.cs
@@ -4,10 +4,7 @@ using Kolaczyn.Application.UseCases;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,14 +15,11 @@ builder.Services.AddTransient<AddUserUseCase>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
   app.UseSwaggerUI();
 }
-
-// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
